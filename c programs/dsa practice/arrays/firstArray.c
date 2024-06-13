ed7f4bc204657b25e52957e564112e6768139b5f
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-06-13 13:56:56 +0530

    added array folder

---
+++ c programs/dsa practice/arrays/firstArray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    int name[50];
+    printf("hello welcome here, enter your name: \n");
+    scanf("%s", name);
+    printf("hello %s", name);
+    return 0;
+}
